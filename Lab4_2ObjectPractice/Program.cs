d3ff5ffde76cc207c5491179e43eff5c9248e98b
Adam Rolain <AdamRolain@QuickenLoans.com>
2021-08-03 15:03:53 -0400

    Initializing repo

---
+++ Lab4_2ObjectPractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab4_2ObjectPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
